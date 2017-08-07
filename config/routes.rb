d7850b050dcfca48e4ea13b1b6ffac38877141e2
tako <rina.m.takase@gmail.com>
2017-08-07 23:27:21 +0900

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
+# == Route Map
+#
+#   Prefix Verb URI Pattern         Controller#Action
+# callback POST /callback(.:format) report#callback
+# decision POST /decision(.:format) report#decision
+#     auth GET  /auth(.:format)     report#auth
+# 
+
 Rails.application.routes.draw do
-  get 'report/new'
+  post '/callback', to: 'report#callback'
+  post '/decision', to: 'report#decision'
+  get '/auth', to: 'report#auth'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
